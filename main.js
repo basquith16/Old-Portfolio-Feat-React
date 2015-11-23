6dc67c43874f15c3f194a3bbf6c173d6d2273a3b
Brian M Asquith <basquithcpt@gmail.com>
2015-11-22 22:23:59 -0500

    building routes

--- main.js
+++ main.js
@@ -3,22 +3,34 @@
 class Router extends Backbone.Router {
   get routes() {
     return {
-routes: {
-      '': 'index',
-      ':blog': 'showBlog',
-      ':about': 'showAbout',
-      ':portfolio': 'showPortfolio'
+      routes: {
+        '': 'index',
+        ':blog': 'showBlog',
+        ':about': 'showAbout',
+        ':portfolio': 'showPortfolio'
+      }
     }
   }
-}
-    showBlog() {
-      $.get('blog.html').then(function(page) {
-        $('.content').html(page);
-          })
-      }
-  };
+  showBlog() {
+    console.log('showBlog');
+    $.get('blog.html').then(function(page) {
+      $('#content').html(page);
+    })
+  }
+  showAbout() {
+    $.get('about.html').then(function(page) {
+      $('#content').html(page);
+    })
+  }
+  showPortfolio() {
+    $.get('portfolio.html').then(function(page) {
+      $('#content').html(page);
+    })
+  }
+};
 
-  $(function() {
-    const router = new Router();
-    Backbone.history.start();
-  });
+$(function() {
+  const router = new Router();
+  Backbone.history.start();
+  console.log('history started');
+});
